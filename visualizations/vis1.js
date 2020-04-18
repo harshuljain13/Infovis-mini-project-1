f71e3b2c11573ac4e95a8acb065fb904f2281c20
harshul1610 <harshulrobo@gmail.com>
2020-04-18 12:49:56 -0400

    adding the ramp function

--- visualizations/vis1.js
+++ visualizations/vis1.js
@@ -1,3 +1,14 @@
+function ramp(color, n = 256) {
+  const canvas = DOM.canvas(n, 1);
+  const context = canvas.getContext("2d");
+  for (let i = 0; i < n; ++i) {
+    context.fillStyle = color(i / (n - 1));
+    context.fillRect(i, 0, 1, 1);
+  }
+  return canvas;
+}
+
+
 function legend({
   div,
   color,
